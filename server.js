9004a1250c94020d53edc9f87f71e7bda913ceb3
Hemant Aggarwal <agarwalhemant212@gmail.com>
2026-05-11 22:26:04 +0530

    Format files and fix uncommitted changes

--- server.js
+++ server.js
@@ -41,11 +41,7 @@ app.use((req, res) => {
 });
 
 // start server
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
